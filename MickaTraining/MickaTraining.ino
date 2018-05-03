882b60d29c3d23774a3b5857030ab808183d649e
IROULIN Jonathan <jonathan.iroulin@universcience.fr>
2018-05-03 21:25:40 +0200

    Initialisation du projet pour Micka

---
+++ MickaTraining/MickaTraining.ino
@@ -0,0 +1,183 @@
+/**
+  Projet Bad Gargenville
+  Name: GargenvilleBadmintonReflexTime
+  Purpose: Dispositif d'entrainement pour le collectif Perf MPG géré par Micka
+
+  @author Jonathan IROULIN
+  @version 1.0 03/05/18
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+/**
+  Démarre le jeu.
+  
+*/
+void demarrerLeJeu() {
+  
+}
+
+/**
+  Allume les deux voyants.
+  
+*/
+void allumerLesDeuxVoyants() {
+  
+}
+
+/**
+  Eteindre les deux voyants.
+  
+*/
+void eteindreLesDeuxVoyants() {
+  
+}
+
+/**
+  Allume le voyant à jouer.
+
+  @param idVoyant identifiant du voyant à allumer.
+  
+*/
+void allumerLeVoyant(int idVoyant) {
+  
+}
+
+/**
+  Eteindre le voyant à jouer
+
+  @param idVoyant identifiant du voyant à eteindre.
+  
+*/
+void eteindreLeVoyant(int idVoyant) {
+  
+}
+
+/**
+  Allume le bouton lumineux à jouer.
+
+  @param idBoutonLumineux identifiant du bouton lumineux à allumer.
+  
+*/
+void allumerBoutonLumineux(int idBoutonLumineux) {
+  
+}
+
+/**
+  Eteindre le bouton lumineux à jouer.
+
+  @param idBoutonLumineux identifiant du bouton lumineux à eteindre.
+  
+*/
+void eteindreBoutonLumineux(int idBoutonLumineux) {
+  
+}
+
+/**
+  Allume le voyant et le bouton lumineux à jouer.
+
+  @param idVoyantEtBoutonLumineux identifiant du voyant et bouton lumineux à allumer.
+  
+*/
+void allumerLeVoyantEtBouton(int idVoyantEtBoutonLumineux) {
+  
+}
+
+/**
+  Eteindre le voyant et le bouton lumineux à jouer.
+
+  @param idVoyantEtBoutonLumineux identifiant du voyant et bouton lumineux à eteindre.
+  
+*/
+void eteindreLeVoyantEtBouton(int idVoyantEtBoutonLumineux) {
+  
+}
+
+/**
+  Décrémente de un le nombre de point à jouer.
+  
+*/
+void decrementerLesAppuisRestants() {
+  
+}
+
+/**
+  choisi un bouton au hasard.
+  
+*/
+void choisirBoutonHasard() {
+  
+}
+
+/**
+  Attend la temporisation si besoin.
+  
+*/
+void attendreTemporisation() {
+  
+}
+
+/**
+  Démarre le programme.
+  
+*/
+void testerNombreAppuiRestant() {
+  
+}
+
+/**
+  Démarre le temps de jeu.
+  
+*/
+void demarrerLeTempsDeJeu() {
+  
+}
+
+/**
+  Pause le temps de jeu.
+  
+*/
+void pauserLeTempsDeJeu() {
+  
+}
+
+/**
+  Arrête le temps de jeu.
+  
+*/
+void arreterLeTempsDeJeu() {
+  
+}
+
+/**
+  Calculer le temps moyen.
+  
+*/
+void calculerTempsMoyen() {
+  
+}
+
+/**
+  Affiche le temps mis.
+  
+*/
+void afficherLeTemps() {
+  
+}
+
+/**
+  Remet le jeu à zéro avant redemarrage.
+  
+*/
+void remettreAZero() {
+  
+}
